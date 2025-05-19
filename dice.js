1a52cde355029ef0632869f36b87c5e6e6a34c75
Vettas Alexandros <alexandros.vettas@gmail.com>
2025-05-19 15:09:09 +0300

    3 files adjusted
    Emoji Change on JS,
    Css -> no wrap
    HTML added title

--- dice.js
+++ dice.js
@@ -16,9 +16,9 @@ function rollDice() {
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🥇 Player 1 Wins! 🥈";
   } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "🥈Player 2 Wins! 🥇";
   } else {
     document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
   }
